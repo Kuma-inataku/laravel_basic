e84832698ccd5a3e206630ab823a80223eea5138
kuma <inataku0121@gmail,com>
2021-08-27 09:01:28 +0900

    [add]simply service container

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\Comment;
+use App\MyClasses\MyService as MyClassesMyService;
 use Illuminate\Http\Request;
+use App\MyClasses\MyService;
 
 class CommentController extends Controller
 {
@@ -10,5 +12,15 @@ class CommentController extends Controller
         $comments = Comment::get();
         dump($comments);
         return view('comments.index',['comments' => $comments]);
+    }
+
+    // public function myservice(MyService $myservice){
+    public function myservice(){
+        $myservice = app('App\MyClasses\MyService');
+        $data = [
+            'msg' => $myservice->say(),
+            'data' => $myservice->data(),
+        ];
+        return view('myservice.index',$data);
     }
 }
