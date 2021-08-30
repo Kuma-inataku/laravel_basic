0c11c806e12e32e7eec0d62a831eb5240ebc9365
kuma <inataku0121@gmail,com>
2021-08-31 08:43:21 +0900

    [add]bind service

--- app/MyClasses/MyService.php
+++ app/MyClasses/MyService.php
@@ -12,16 +12,23 @@ class MyService
 
   public function __construct(int $id = -1)
   {
-    if($id >= 0)
-    {
-      $this->id = $id;
-      $this->msg = 'select' . $this->data[$id];
-    }
   }
 
+  public function setId($id){
+    $this->id = $id;
+    if($id >= 0 && $id < count($this->data))
+    {
+      $this->msg = 'select id :' . $id . ', data :"' . $this->data[$id].'"';
+    }
+  }
   public function say()
   {
     return $this->msg;
+  }
+
+  public function data(int $id)
+  {
+    return $this->data[$id];
   }
 
   public function alldata(){
