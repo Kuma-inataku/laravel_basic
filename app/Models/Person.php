90d5bd03fcdd535b727f422c3a2e6b3406d97e4d
kuma <inataku0121@gmail,com>
2021-09-14 09:05:33 +0900

    add global scope

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
@@ -13,16 +14,29 @@ class Person extends Model
     {
        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
+
     public function scopeNameEqual($query, $str)
     {
        return $query->where('name', $str);
     }
+
     public function scopeAgeGreaterThan($query, $n)
     {
         return $query->where('age','>=', $n);
     }
+
     public function scopeAgeLessThan($query, $n)
     {
         return $query->where('age','<=', $n);
     }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age',function (Builder $builder){
+            $builder->where('age','>',20);
+        });
+    }
+
 }
