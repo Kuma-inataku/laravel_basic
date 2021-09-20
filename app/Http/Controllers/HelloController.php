ce198484655cb0ac2d8439edc9df3bc7baaf98c7
kuma <inataku0121@gmail,com>
2021-09-20 19:23:26 +0900

    use extend method

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,16 +9,6 @@ use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
-    // public function index(Request $request)
-    // {
-    //     return view('hello.index', ['msg' => 'フォームを入力してください']);
-    // }
-
-    // public function post(Request $request)
-    // {
-    //     return view('hello.index', ['msg' => '正しく入力されました。']);
-    // }
-
     public function index(Request $request)
     {
         return view('hello.index', ['msg'=>'フォームを入力下さい。']);
