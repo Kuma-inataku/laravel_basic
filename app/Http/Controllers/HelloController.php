2f51c0c9f08bd68221a6d1086f26a7eb9620f0bd
kuma <inataku0121@gmail,com>
2021-09-20 17:44:04 +0900

    change error message en to ja

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,7 +11,6 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        dump($request->query());
         $validator = Validator::make($request->query(),[
             'id' => 'required',
             'pass' => 'required',
@@ -26,11 +25,18 @@ class HelloController extends Controller
 
     public function post(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150',
-        ]);
+        ];
+        $messages = [
+            'name.required' => '名前は必ず入力してください',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入してください。',
+            'age.between' => '年齢は0~150の間で入力してください',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
         if($validator->fails()){
             return redirect('/hello')
                 ->withErrors($validator)
