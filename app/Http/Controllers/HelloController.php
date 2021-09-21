dd7cdd32fbfa437a374598fdad2ccaa327c81df5
kuma <inataku0121@gmail,com>
2021-09-21 21:35:04 +0900

    add orderby method

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,7 +13,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->simplepaginate(2);
+        $items = DB::table('people')->orderBy('name','asc')->simplepaginate(2);
         return view('hello.index', ['items'=> $items]);
     }
 
