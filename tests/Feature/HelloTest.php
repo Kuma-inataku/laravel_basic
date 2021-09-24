25edf84027c8d2d55d9721d7d2430622172d5784
kuma <inataku0121@gmail,com>
2021-09-24 09:25:39 +0900

    select address accessing test

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -8,6 +8,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Person;
+use App\Models\User;
+use Carbon\Factory;
 
 class HelloTest extends TestCase
 {
@@ -18,19 +21,26 @@ class HelloTest extends TestCase
      * @test
      * 
      */
+
+     use DatabaseMigrations;
+
     public function testHello()
     {
         $this->assertTrue(true);
         
-        $arr = [];
+        $response = $this->get('/');
+        $response->assertStatus(200);
 
-        $msg = 'Hello';
-        $this->assertEquals('Hello', $msg);
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
 
-        $n = random_int(1, 100);
-        $this->assertLessThan(100, $n);
-        // $response = $this->get('/');
+        $person = factory(User::class)->create();
+        // $person = factory(User::class)->create();
+        $response = $this->actingAs($person)->get('/hello');
+        $response->assertStatus(200);
 
-        // $response->assertStatus(200);
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
+
     }
 }
