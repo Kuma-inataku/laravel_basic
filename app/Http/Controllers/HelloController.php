fc09df75ed290fb8683f6b11ce1fe0a8aecd72dc
kuma <inataku0121@gmail,com>
2021-09-25 18:05:21 +0900

    add limit by using diff method

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -19,13 +19,20 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $msg = 'show people record';
-        $result = Person::get()->reject(function ($person){
+        // 50歳以下
+        $result = Person::get()->filter(function ($person){
+            return $person->age < 50;
+        });
+        // 20歳以下
+        $result2 = Person::get()->filter(function ($person){
             return $person->age < 20;
         });
+        // 50歳以下から20歳以下ではないものを取得＝20～50の人→正解！
+        $result3 = $result->diff($result2);
 
         $data = [
             'msg' => $msg,
-            'data' => $result,
+            'data' => $result2,
         ];
 
         return view('hello.index', $data);
