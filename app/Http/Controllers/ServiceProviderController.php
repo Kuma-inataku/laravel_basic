c8ae062f7e9adc12d793c96200a78c88105f69cf
Kuma-inataku <inataku0121@gmail,com>
2021-11-23 22:29:20 +0900

    自作サービスプロバイダで登録したサービスを利用

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Request;
 class ServiceProviderController extends Controller
 {
     public function index() {
-        $encrypt = app()->make('encrypter');
-        $password = $encrypt->encrypt('password');
-        dd($encrypt->decrypt($password));
+        // $encrypt = app()->make('encrypter');
+        // $password = $encrypt->encrypt('password');
+        // dd($encrypt->decrypt($password));
+        $name = app()->make('myName');
+
+        dd($name);
     }
 }
