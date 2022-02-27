47ec8d0b346f3122a6a00b0d6d067655d9507688
Kuma-inataku <inataku0121@gmail,com>
2022-02-27 09:34:44 +0900

    fileのappuro-do

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
@@ -10,6 +11,12 @@ class UploadController extends Controller
         return view('upload.index');
     }
     public function store(Request $request){
-        dd($request->all());
+        // dd($request->file('file'));
+        $file = $request->file('file');
+        $file->storeAs('', 'test.jpg');
+
+        // Storage::disk('s3')->putFile('/', $file);
+        // /storage/appに保存される
+        // $file->store('');
     }
 }
