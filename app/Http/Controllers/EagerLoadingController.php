0e1836bdf6dd98b25c29e322cbc9f72395fc2644
Kuma-inataku <inataku0121@gmail,com>
2022-03-05 16:01:01 +0900

    微修正

--- app/Http/Controllers/EagerLoadingController.php
+++ app/Http/Controllers/EagerLoadingController.php
@@ -8,8 +8,11 @@ use App\Models\User;
 class EagerLoadingController extends Controller
 {
     public function index() {
-        $users = User::with('userAttribute')->get();
-        // $users = User::all();
+        // $users = User::with('userAttribute')->get();
+        // $users = User::whereBetween('id', [1, 10])->first();
+        // $users = User::whereBetween('id', [1, 10])->find(4);
+        $users = User::all();
+
         $data = [
             'users' => $users,
         ];
