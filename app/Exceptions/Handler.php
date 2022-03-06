feabb0e8d685814490377c2645fbdcaf167c56ea
Kuma-inataku <inataku0121@gmail,com>
2022-03-06 09:12:10 +0900

    テスト対象クラスの作成

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -34,8 +35,14 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
+        $this->reportable(function (PreconditionException $e) {
+            return response()->json(
+                ['message' => trans($e->getMessage())],
+                Response::HTTP_BAD_REQUEST
+            );
         });
     }
 }
--- app/UseCases/AddPointUseCase.php
+++ app/UseCases/AddPointUseCase.php
@@ -2,6 +2,13 @@
 
 namespace App\UseCases;
 
+use App\Exceptions\PreconditionException;
+use App\Models\EloquentCustomer;
+use App\Models\EloquentCustomerPoint;
+use App\Models\PointEvent;
+use App\Services\AddPointService;
+use Carbon\CarbonImmutable;
+use Throwable;
 
 final class AddPointUseCase
 {
@@ -14,5 +21,31 @@ final class AddPointUseCase
         $this->service = $service;
         $this->eloquentCustomer = $eloquentCustomer;
         $this->eloquentCustomerPoint = $eloquentCustomerPoint;
+    }
+
+    /**
+     * @throws Throwable
+     *
+     */
+    public function run(int $customerId, int $addPoint, string $pointEvent, CarbonImmutable $now): int
+    {
+        if ($addPoint <= 0) {
+            throw new PreconditionException('add_point should be equals or greater than 1');
+        }
+
+        if (! $this->eloquentCustomer->where('id', $customerId)->exists()) {
+            $message = sprintf('customers.id:%d does not exists', $customerId);
+            throw new PreconditionException($message);
+        }
+
+        if (! $this->eloquentCustomerPoint->where('customer_id', $customerId)->exists()) {
+            $message = sprintf('customer_points.customer_id:%d does not exists', $customerId);
+            throw new PreconditionException($message);
+        }
+
+        $event = new PointEvent($customerId, $pointEvent, $addPoint, $now);
+        $this->service->add($event);
+
+        return $this->eloquentCustomerPoint->findPoint($customerId);
     }
 }
